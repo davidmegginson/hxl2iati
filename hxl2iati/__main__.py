66f26afcd9c103e1dfc4b42fb6b0a11d1b66a7d6
David Megginson <david.megginson@gmail.com>
2021-02-12 10:58:59 -0500

    Add validation support. Add basic XML writer to catch low-level errors. Make into class. Almost (not quite) valid.

--- hxl2iati/__main__.py
+++ hxl2iati/__main__.py
@@ -1,9 +1,17 @@
-import sys
-from hxl2iati.convert import convert
+import hxl2iati.convert, sys
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: {} <url_or_filename>".format(sys.argv[0]), file=sys.stderr)
         sys.exit(2)
-    convert(sys.argv[1], sys.stdout, True)
+
+    converter = hxl2iati.convert.HXL2IATI(
+        output=sys.stdout,
+        reporting_org_name="OCHA Somalia",
+        recipient_country_name="Somalia",
+        recipient_country_code="SO",
+        default_start_date="2020-11-01",
+        default_update_date="2020-11-30",
+    )
+    converter.convert(sys.argv[1], allow_local=True)
--- hxl2iati/convert.py
+++ hxl2iati/convert.py
@@ -1,21 +1,156 @@
-import datetime, hashlib, hxl
-
-REQUIRED_HASHTAGS = [
-    "#org+impl",
-    "#sector",
-    "#activity",
-    "#adm1+name",
-    "#date+start",
-    "#date+end",
-    "#status",
-]
+import datetime, dateutil, hashlib, hxl, hxl2iati.writer, re
 
 class ConversionException (Exception):
-    pass
 
-def esc (s):
-    """ Escape text for XML content """
-    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&apos;").replace('"', "&quot;")
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class HXL2IATI:
+
+    
+    REQUIRED_HASHTAGS = [
+        "#org+impl",
+        "#sector",
+        "#activity",
+        "#adm1+name",
+        "#date+start",
+        "#date+end",
+        "#status",
+    ]
+    """ Required columns to do this processing """
+
+    
+    def __init__ (self, output, reporting_org_name, recipient_country_name, recipient_country_code, default_start_date, default_update_date):
+        self.reporting_org_name = reporting_org_name
+        self.recipient_country_name = recipient_country_name
+        self.recipient_country_code = recipient_country_code
+        self.default_start_date = default_start_date
+        self.default_update_date = default_update_date
+
+        self.xmlout = hxl2iati.writer.XMLWriter(output=output)
+
+        
+    def convert (self, url_or_filename, allow_local=False):
+        """ Top-level method to convert a HXLated 3W to IATI
+
+        """
+
+        # open the data source (usually a URL)
+        source = hxl.data(url_or_filename, allow_local=allow_local)
+
+        # check that the required hashtags are present
+        check_hashtags(source, self.REQUIRED_HASHTAGS)
+
+        self.xmlout.start_document()
+
+        self.xmlout.start_block("iati-activities", {
+            "generated-datetime": datetime.datetime.now().isoformat(),
+            "version": "2.03",
+        })
+
+        for row in source:
+            self.do_activity(row)
+
+        self.xmlout.end_block("iati-activities")
+
+        self.xmlout.end_document()
+
+        
+    def do_activity(self, row):
+        """ convert a HXLated 3W row to an iati-activity """
+
+        # TODO send the 3W's actual datetime
+        self.xmlout.start_block("iati-activity", {
+            "xml:lang": "en",
+            "default-currency": "USD",
+            "last-updated-datetime": self.default_update_date,
+            "humanitarian": "1",
+        })
+
+        self.xmlout.simple_element("iati-identifier", content=make_identifier(row))
+
+        # TODO make reporting org configurable
+        self.do_narrative("reporting-org", {
+            "ref": "TODO",
+            "type": "TODO",
+        }, self.reporting_org_name)
+
+        if row.get("#activity"):
+            self.do_narrative("title", {}, row.get("#activity"))
+            self.do_narrative("description", {}, row.get("#activity"))
+
+        if row.get("#org+impl"):
+            self.do_narrative("participating-org", {
+                "role": "4",
+                "ref": "TODO",
+                "type": "TODO",
+            }, row.get("#org+impl"))
+
+        # TODO add org type
+        if row.get("#org+prog"):
+            self.do_narrative("participating-org", {
+                "role": "2",
+                "ref": "TODO",
+                "type": "TODO",
+            }, row.get("#org+prog"))
+
+        if row.get("#org+funding"):
+            self.do_narrative("participating-org", {
+                "role": "1",
+                "ref": "TODO",
+                "type": "TODO",
+            }, row.get("#org+funding"))
+
+        self.xmlout.simple_element("activity-status", {
+            "code": row.get("#status", default=""),
+        })
+
+        d = fix_date(row.get("#date+start"), self.default_start_date)
+        if d:
+            self.xmlout.simple_element("activity-date", {
+                "iso-date": d,
+                "type": "2",
+            })
+
+        d = fix_date(row.get("#date+end"), self.default_start_date)
+        if d:
+            self.xmlout.simple_element("activity-date", {
+                "iso-date": d,
+                "type": "4",
+            })
+
+        self.do_narrative("recipient-country", {
+            "code": self.recipient_country_code,
+            "percentage": "100",
+        }, self.recipient_country_name)
+
+        if row.get("#sector"):
+            # TODO add sector code
+            self.do_narrative("sector", {
+                "code": "TODO",
+            }, row.get("#sector"))
+
+        self.xmlout.end_block("iati-activity")
+
+        
+    def do_narrative (self, name, atts={}, content=None):
+        if content is None:
+            self.xmlout.simple_element(name, atts)
+        else:
+            self.xmlout.start_block(name, atts)
+            self.xmlout.simple_element("narrative", content=content)
+            self.xmlout.end_block(name)
+
+
+# Helper functions
+
+def fix_date (s, default_date=None):
+    try:
+        return dateutil.parser.parse(s).isoformat()[:10]
+    except:
+        return default_date
+
 
 def check_hashtags (source, hashtags):
     """ Check that all the required hashtags are present.
@@ -28,81 +163,11 @@ def check_hashtags (source, hashtags):
     if missing_columns:
         raise ConversionException("Missing column(s): {}".format(", ".join(missing_columns)))
 
+
 def make_identifier (row):
     # FIXME not really reliable
     hash = hashlib.md5("|||".join(row.values).encode("utf-8"))
     return "OCHA-3W-SOM-{}".format(hash.hexdigest())
 
-def start_xml (output):
-    """ XML frontmatter """
-    print("<?xml version=\"1.0\"?>\n\n<iati-activities generated-datetime=\"{}\" version=\"2.03\">".format(datetime.datetime.now().isoformat()), file=output)
 
-def end_xml(output):
-    """ XML backmatter """
-    print("</iati-activities>", file=output)
-
-def narrative_el(tag, content, output, code=None):
-    """ Display a narrative text element, optionally with a code attribute """
-    code_string = ""
-    if code is not None:
-        code_string = " code=\"{}\"".format(esc(str(code)))
-    print("    <{tag}{code_string}>\n      <narrative>{content}</narrative>\n    </{tag}>".format(tag=tag, code_string=code_string, content=esc(content)), file=output)
-
-def do_activity(row, output):
-    """ convert a HXLated 3W row to an iati-activity """
-
-    # TODO send the 3W's actual datetime
-    print("  <iati-activity xml:lang=\"en\" default-currency=\"USD\" last-updated_datetime=\"{}\">".format(esc(datetime.datetime.now().isoformat())), file=output)
-
-    print("    <iati-identifier>{}</iati-identifier>".format(esc(make_identifier(row))), file=output)
-
-    # TODO make reporting org configurable
-    narrative_el("reporting-org", "OCHA Somalia", output)
-
-    if row.get("#org+impl"):
-        narrative_el("participating-org", row.get("#org+impl"), output, code=4)
-
-    # TODO add org type
-    if row.get("#org+prog"):
-        narrative_el("participating-org", row.get("#org+prog"), output, code=2)
-
-    if row.get("#org+funding"):
-        narrative_el("participating-org", row.get("#org+funding"), output, code=1)
-
-    if row.get("#activity"):
-        narrative_el("title", row.get("#activity"), output)
-
-    if row.get("#sector"):
-        # TODO add sector code
-        narrative_el("sector", row.get("#sector"), output)
-
-    if row.get("#date+start"):
-        # TODO check date format
-        print("    <activity_date iso_date=\"{}\" code=\"2\"/>".format(esc(row.get("#date+start"))), file=output)
-
-    if row.get("#date+end"):
-        # TODO check date format
-        print("    <activity_date iso_date=\"{}\" code=\"4\"/>".format(esc(row.get("#date+end"))), file=output)
-
-    # TODO make configurable
-    print("    <recipient-country code=\"SO\" percentage=\"100\"/>", file=output)
-
-    print("  </iati-activity>", file=output)
-
-def convert (url_or_filename, output, allow_local=False):
-    """ Top-level function to convert HXL to IATI
-    """
-
-    # open the data source (usually a URL)
-    source = hxl.data(url_or_filename, allow_local=allow_local)
-
-    # check that the required hashtags are present
-    check_hashtags(source, REQUIRED_HASHTAGS)
-
-    start_xml(output)
-
-    for row in source:
-        do_activity(row, output)
-
-    end_xml(output)
-    
+# end
